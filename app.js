c563985c578017c3315616bd4dcb706bf34f896b
backas36 <backas36@gmail.com>
2022-01-15 17:55:33 +0800

    methos used for data binding, and talk about method in html performance

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const app = Vue.createApp({
     resetInput() {
       this.name = "";
     },
+    outputFullName(lastname) {
+      console.log("fire"); //每一次都會觸發，即使沒有更新，例如 按了 counter add 10 button，畫面看起來是沒變的，但其實每一次都是新的 method
+      return this.name ? `${this.name} ${lastname}` : "";
+    },
   },
 });
 

4f14fc2a329e562d24222fdb12e40162d03c51e7
backas36 <backas36@gmail.com>
2022-01-15 14:58:14 +0800

    finished lesson24

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "Ashi",
     };
   },
   methods: {
     reduceCounter() {
       this.counter--;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
     },
   },
 });

9e97b36a6dc89bbb96e63c08b7b77fdd28e32562
backas36 <backas36@gmail.com>
2022-01-17 00:11:54 +0800

    core function in game done

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
 const getRandomValue = (max, min) => {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 };
+
 const app = Vue.createApp({
   data() {
     return {
       monsterHealth: 100,
       playerHealth: 100,
+      currentRound: 0,
+      result: null,
     };
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        //draw
+        this.result = "draw";
+      } else if (value <= 0) {
+        //Player lost
+        this.result = "Monster";
+      } else {
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        //draw
+        this.result = "draw";
+      } else if (value <= 0) {
+        //Monster lost
+        this.result = "Player";
+      } else {
+      }
+    },
+  },
+  computed: {
+    monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.playerHealth + "%" };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(10, 5);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -18,5 +61,29 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(15, 8);
       this.playerHealth -= attackValue;
     },
+    // every three round available
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(25, 10);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healVaule = getRandomValue(20, 8);
+      if (this.playerHealth + healVaule > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healVaule;
+      }
+      this.attackPlayer();
+    },
+    restartGame() {
+      (this.monsterHealth = 100),
+        (this.playerHealth = 100),
+        (this.currentRound = 0),
+        (this.result = null);
+    },
+    surrender() {},
   },
 }).mount("#game");

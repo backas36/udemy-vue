84d648598aa7875a1bbb18c7a5950b0c95c9eaea
backas36 <backas36@gmail.com>
2022-01-16 13:12:50 +0800

    dynamic styling with inline styles

--- app.js
+++ app.js
@@ -1,57 +1,21 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
-      name: "",
-      //fullName: "",
-      lastName: "",
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
     };
   },
-  watch: {
-    counter(value) {
-      if (value > 20) {
-        setTimeout(() => {
-          this.counter = 0;
-          console.log(this);
-        }, 1000);
-      }
-    },
-    //name(value, oldValue) {
-    //  //this.fullName = this.name + " " + "Yang";
-    //  //參數 value 就是綁定的 property : name
-    //  if (value === "") {
-    //    this.fullName = "";
-    //  } else {
-    //    this.fullName = value + " " + this.lastName;
-    //  }
-    //  console.log(oldValue);
-    //},
-    //lastName(value) {
-    //  if (value === "") {
-    //    this.lastName = "";
-    //  } else {
-    //    this.fullName = this.name + " " + value;
-    //  }
-    //},
-  },
-  computed: {
-    fullName() {
-      console.log("fire fullName");
-      return this.name ? `${this.name} ${this.lastName}` : "";
-    },
-  },
   methods: {
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    },
-    resetInput() {
-      this.name = "";
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxASelected = !this.boxASelected;
+      } else if (box === "B") {
+        this.boxBSelected = !this.boxBSelected;
+      } else {
+        this.boxCSelected = !this.boxCSelected;
+      }
     },
   },
 });
-
-app.mount("#events");
+app.mount("#styling");

2da9fbbc285c2caf12294e633b5d8ebd69e5f895
backas36 <backas36@gmail.com>
2022-01-16 14:47:40 +0800

    start from Chapter3 Rendering Conditional Content&Lists

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const app = Vue.createApp({
   data() {
-    return {
-      counter: 0,
-    };
-  },
-  methods: {
-    reduceCounter() {
-      this.counter--;
-    },
+    return { goals: [] };
   },
 });
 
-app.mount("#events");
+app.mount('#user-goals');
